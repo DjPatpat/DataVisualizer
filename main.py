eb352fadd3c6e217e7ffd3848c469bb568a11d68
Patrick <patrick.payer@technikum-wien.at>
2025-07-23 15:25:14 +0200

    Testing writing labels and button interactions.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk     # import Tk themed widgets (new widgets from 2007)
 
 class App(tk.Tk):
     def __init__(self):
@@ -18,6 +19,27 @@ class App(tk.Tk):
 
         # set the position of the window to the center of the screen
         self.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+        # test run of setting a label
+        label = ttk.Label(self)
+        label.config(text='Themed label')
+        label.pack()
+
+        # first test of a button with action
+        # pass command function without parentheses () to not call it at the start of the program
+        ttk.Button(self, text='Click Me', command=button_clicked).pack()
+
+        # Ensure icon is in the assets folder
+        # TODO: create assets folder
+        # TODO: prepare icon in .ico format
+        # self.iconbitmap('./assets/pythontutorial.ico')
+
+
+# test function for button interaction
+# arguments can be passed using lambda expressions, see
+# https://www.pythontutorial.net/tkinter/tkinter-command/
+def button_clicked():
+    print('Button clicked')
 
 
 if __name__ == '__main__':
