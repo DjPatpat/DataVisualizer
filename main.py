2b97c0e56dcd0ec34b9e9e488cbdb41c97e97986
Patrick <patrick.payer@technikum-wien.at>
2025-07-23 14:55:49 +0200

    Added centering of window, resized intial window.

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ import tkinter as tk
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.title('data visualizer')
+
+        window_width = 600
+        window_height = 400
+
+        # get the screen dimension
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+
+        # find the center point
+        center_x = int(screen_width / 2 - window_width / 2)
+        center_y = int(screen_height / 2 - window_height / 2)
+
+        # set the position of the window to the center of the screen
+        self.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
 
 if __name__ == '__main__':
