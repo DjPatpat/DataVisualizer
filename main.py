b6dec47a98a912023c76b56ed3d4174d5f9d3810
Patrick <patrick.payer@technikum-wien.at>
2025-07-24 12:25:47 +0200

    Moved window sizing to an own function, added parameters for initial size.

--- main.py
+++ main.py
@@ -2,13 +2,29 @@ import tkinter as tk
 from tkinter import ttk     # import Tk themed widgets (new widgets from 2007)
 
 class App(tk.Tk):
-    def __init__(self):
+    def __init__(self, width, height):
         super().__init__()
-        self.title('data visualizer')
+        self.title('DataVisualizer')
+        self.__set_size(width, height)
 
-        window_width = 600
-        window_height = 400
+        # test run of setting a label
+        label = ttk.Label(self)
+        label.config(text='Themed label')
+        label.pack()
 
+        # first test of a button with action
+        # pass command function without parentheses () to not call it at the start of the program
+        button = ttk.Button(self, text='Click Me', command=button_clicked)
+        button.place(anchor='nw')
+        button.state(['!disabled'])
+
+        # Ensure icon is in the assets folder
+        # TODO: create assets folder
+        # TODO: prepare icon in .ico format
+        # self.iconbitmap('./assets/pythontutorial.ico')
+
+
+    def __set_size(self, window_width, window_height):
         # get the screen dimension
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
@@ -20,20 +36,6 @@ class App(tk.Tk):
         # set the position of the window to the center of the screen
         self.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
-        # test run of setting a label
-        label = ttk.Label(self)
-        label.config(text='Themed label')
-        label.pack()
-
-        # first test of a button with action
-        # pass command function without parentheses () to not call it at the start of the program
-        ttk.Button(self, text='Click Me', command=button_clicked).pack()
-
-        # Ensure icon is in the assets folder
-        # TODO: create assets folder
-        # TODO: prepare icon in .ico format
-        # self.iconbitmap('./assets/pythontutorial.ico')
-
 
 # test function for button interaction
 # arguments can be passed using lambda expressions, see
@@ -43,5 +45,5 @@ def button_clicked():
 
 
 if __name__ == '__main__':
-    app = App()
+    app = App(width=600, height=400)
     app.mainloop()
