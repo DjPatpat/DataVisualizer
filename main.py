3d43dc549638685272c96cae513c92968a785588
DjPatpat <BlackPatrick160@gmail.com>
2025-07-24 15:19:06 +0200

    Update Buttons > Added General Purpose Buttons (no functionality) > Added Specific Buttons (no functionality) > Added Themes

--- main.py
+++ main.py
@@ -12,10 +12,55 @@ class ButtonGeneral(ttk.Frame):
 
 
     def __create_widgets(self):
-        ttk.Button(self, text="1").grid(row=0, column=0)
-        ttk.Button(self, text="2").grid(row=1, column=0)
-        ttk.Button(self, text="3").grid(row=2, column=0)
-        ttk.Button(self, text="4").grid(row=3, column=0)
+        ttk.Button(self, text="Save").grid(row=0, column=0)
+        ttk.Button(self, text="Save as").grid(row=1, column=0)
+        ttk.Button(self, text="Load").grid(row=2, column=0)
+        ttk.Button(self, text="Export").grid(row=3, column=0)
+        ttk.Button(self, text="LaTeX").grid(row=4, column=0)
+        ttk.Button(self, text="PDF").grid(row=5, column=0)
+
+
+class ButtonSpecific(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+
+        # grid layout manager
+        self.columnconfigure(index=1, weight=1)
+
+        self.__create_widgets()
+
+
+    def __create_widgets(self):
+        ttk.Button(self, text="Choose Columns").grid(row=0, column=1)
+        ttk.Button(self, text="Single Plot").grid(row=0, column=2)
+        ttk.Button(self, text="Add Column").grid(row=0, column=3)
+        ttk.Button(self, text="Remove Column").grid(row=0, column=4)
+        ttk.Button(self, text="Add Row").grid(row=0, column=5)
+        ttk.Button(self, text="Remove Row").grid(row=0, column=6)
+
+
+class ButtonThemes(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+
+        container.style = ttk.Style(container)
+        container.selected_theme = tk.StringVar()
+
+        def change_theme():
+            container.style.theme_use(container.selected_theme.get())
+
+        theme_frame = ttk.LabelFrame(self, text="Theme")
+        theme_frame.grid(row=2, column=0)
+
+        for theme_name in container.style.theme_names():
+            rb = ttk.Radiobutton(
+                theme_frame,
+                text=theme_name,
+                value=theme_name,
+                variable=container.selected_theme,
+                command=change_theme
+            )
+            rb.pack(expand=True, anchor='sw')
 
 
 class App(tk.Tk):
@@ -56,7 +101,13 @@ class App(tk.Tk):
 
     def __create_widgets(self):
         button_general = ButtonGeneral(self)
-        button_general.grid(column=0, row=0, columnspan=2)
+        button_general.grid(column=0, row=0, rowspan=2, sticky=tk.NW)
+
+        button_specific = ButtonSpecific(self)
+        button_specific.grid(column=1, row=0, columnspan=2, sticky=tk.N)
+
+        button_themes = ButtonThemes(self)
+        button_themes.grid(column=0, row=2, sticky=tk.SW)
 
 
 # test function for button interaction
